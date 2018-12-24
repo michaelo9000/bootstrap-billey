c825d9324cae3144e7008dced78ccd612ec33219
Michael <michael.ohara@hotmail.co.nz>
2018-12-24 14:02:01 +1300

    move PlayerWeapon, make it interface through References

---
+++ Assets/Player/Scripts/PlayerWeapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour{
+
+    PlayerReferences References;
+    void Start()
+    {
+        References = GetComponent<PlayerReferences>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var healthToRestore = References._CollisionKeeper.DamageAllEnemyCollisions(5);
+        References._HealthAndDamage.UpdateHealthAndStamina(healthToRestore, 0);
+    }
+}
